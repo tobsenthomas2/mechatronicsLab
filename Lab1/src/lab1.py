b2dd50b817c5a9d079b38d0dcec2595f5206fbf6
sydneygoth <122558998+sydneygoth@users.noreply.github.com>
2023-01-26 09:16:44 -0800

    Update lab1.py

--- Lab1/src/lab1.py
+++ Lab1/src/lab1.py
@@ -7,6 +7,42 @@ The LED will be from off to on in 5 secudes and the brighntes will increase ever
 """
 
 """! this imports the libraries, pyb and time """
+"""!
+Lab1 Documentation 
+Team mecha02: Sydney Gothenquist, Toby Moss Darci-Maher, Tobias Thomas 
+
+In this two-week exercise, you will write code which runs a motor and measures 
+where it is, and then you’ll encapsulate that code in Python classes. You will get
+practice documenting code with Doxygen, testing your code thoroughly to ensure that
+it is reliable, and managing source code files using Git.
+
+class MotorDriver:
+    """! 
+    This class implements a motor driver for an ME405 kit. 
+    """
+
+    def __init__ (self, en_pin, in1pin, in2pin, timer):
+        """! 
+        Creates a motor driver by initializing GPIO
+        pins and turning off the motor for safety. 
+        @param en_pin (There will be several pin parameters)
+        """
+        print ("Creating a motor driver")
+
+    def set_duty_cycle (self, level):
+        """!
+        This method sets the duty cycle to be sent
+        to the motor to the given level. Positive values
+        cause torque in one direction, negative values
+        in the opposite direction.
+        @param level A signed integer holding the duty
+               cycle of the voltage sent to the motor 
+        """
+        print (f"Setting duty cycle to {level}")
+        
+    if __name__ == '__main__'
+     
+
 import pyb
 import time
 
