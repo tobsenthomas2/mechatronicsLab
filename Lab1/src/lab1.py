fb6e2a2910a7141e28820decfbf381e77d644db6
sydneygoth <sgothenquist@hotmail.com>
2023-01-26 09:55:18 -0800

    Update lab1.py
    timer channel stuff??

--- Lab1/src/lab1.py
+++ Lab1/src/lab1.py
@@ -1,11 +1,8 @@
 """!
-Lab0 Documentation 
+Lab1 Documentation 
 Team mecha02: Sydney Gothenquist, Toby Moss Darci-Maher, Tobias Thomas 
- 
-The Program should lead to a dimming LED controlled by a STM32 and written in micropython. 
-The LED will be from off to on in 5 secudes and the brighntes will increase every 0.05 secundes
+
 """
-sdfklsdlkf
 """! this imports the libraries, pyb and time """
 """!
 Lab1 Documentation 
@@ -15,11 +12,30 @@ In this two-week exercise, you will write code which runs a motor and measures
 where it is, and then you’ll encapsulate that code in Python classes. You will get
 practice documenting code with Doxygen, testing your code thoroughly to ensure that
 it is reliable, and managing source code files using Git.
+"""
+class encoder:
+    def __init__ (self,CH1,CH2):
+        #pb6/7 t4c1orc2
+        
+        #timer channel 4
+        timer = pyb.Timer(4)
+        
+        #sets up channel 1&2 for encoder A&B
+        timer_ch1 = timer.channel(1,pyb.Timer.ENC_A)
+        timer_ch2 = timer.channel(2,pyb.Timer.ENC_B)
+        
+        #set pins for PB6/7
+        pinPB6 = pyb.Pin (pyb.Pin.board.PB6, pyb.Pin.OUT_PP)
+        pinPB7 = pyb.Pin (pyb.Pin.board.PB7, pyb.Pin.OUT_PP)
+        
+        """!Reconnect USB and write code that sets up a timer and
+its CH1 and CH2 channels in encoder counting mode.
+Only one timer will work with those pins;"""
+        self.CH1 = 
 
 class MotorDriver:
-    """! 
-    This class implements a motor driver for an ME405 kit. 
-    """
+    
+    #This class implements a motor driver for an ME405 kit. 
 
     def __init__ (self, en_pin, in1pin, in2pin, timer):
         """! 
@@ -43,56 +59,56 @@ class MotorDriver:
     if __name__ == '__main__'
      
 
-import pyb
-import time
-
-def led_setup ():
-"""! init function for all Pins.
-
-This function, led_setup, sets up the pins for timer 2, channel 1
-the A0 pin functions as an output with input from the 3v3 port for the LED.
-
-    """
-    
-    pinA0 = pyb.Pin (pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
-    tim2 = pyb.Timer (2, freq=20000)
-    
-    global ch2
-    ch2 = tim2.channel (1, pyb.Timer.PWM, pin=pinA0)
-
-def led_brightness (perc):
-"""! controll function needs brightness percentage as input
-
-this function, led_brightness defines the brightness of the led as set by the PWM duty cycle.
-this duty cycle ranges from 0-100%, and is defined by "value" in the main function
-    """
-
-    global ch2
-    #pyb.Timer.PWM_INVERTED # did not work for us, so we implemented a workaround
-    ch2.pulse_width_percent (perc)
-  
-if __name__ == "__main__":
-"""!in this main function, we are running the seperately defined functions, led_setup and led_brightness 
-    """
-    led_setup()
-    time.sleep(1)
-    """! by setting i = 0, this creates an infinite loop so that the LED will keep looping until
-    the program is stopped since the while loop will only stop if i > 10 """
-    
-    
-    i=0  
-    while i<=10:
-    """! sets the value to what is specified and decrements down from 100 to 0, as we did not use the INVERTED function
-        the range for value is set from 0-100 so that it cannot go out of this range.
-        100 = dimmest and 0 = brightest as per the not inverted PWM """
-        
-        for  value in range(100,0,-1):
-        """! the led brightness is equivalent to the specified value as defined by the for loop """
-            
-            led_brightness(value)
-            
-            """! each run through this for loop will be for 0.05s. Since it is running 100x, this equates
-            to a total runtime of 5s as specified in the lab """
-            
-            time.sleep(0.05)
-     
+# import pyb
+# import time
+# 
+# def led_setup ():
+# """! init function for all Pins.
+# 
+# This function, led_setup, sets up the pins for timer 2, channel 1
+# the A0 pin functions as an output with input from the 3v3 port for the LED.
+# 
+#     """
+#     
+#     pinA0 = pyb.Pin (pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+#     tim2 = pyb.Timer (2, freq=20000)
+#     
+#     global ch2
+#     ch2 = tim2.channel (1, pyb.Timer.PWM, pin=pinA0)
+# 
+# def led_brightness (perc):
+# """! controll function needs brightness percentage as input
+# 
+# this function, led_brightness defines the brightness of the led as set by the PWM duty cycle.
+# this duty cycle ranges from 0-100%, and is defined by "value" in the main function
+#     """
+# 
+#     global ch2
+#     #pyb.Timer.PWM_INVERTED # did not work for us, so we implemented a workaround
+#     ch2.pulse_width_percent (perc)
+#   
+# if __name__ == "__main__":
+# """!in this main function, we are running the seperately defined functions, led_setup and led_brightness 
+#     """
+#     led_setup()
+#     time.sleep(1)
+#     """! by setting i = 0, this creates an infinite loop so that the LED will keep looping until
+#     the program is stopped since the while loop will only stop if i > 10 """
+#     
+#     
+#     i=0  
+#     while i<=10:
+#     """! sets the value to what is specified and decrements down from 100 to 0, as we did not use the INVERTED function
+#         the range for value is set from 0-100 so that it cannot go out of this range.
+#         100 = dimmest and 0 = brightest as per the not inverted PWM """
+#         
+#         for  value in range(100,0,-1):
+#         """! the led brightness is equivalent to the specified value as defined by the for loop """
+#             
+#             led_brightness(value)
+#             
+#             """! each run through this for loop will be for 0.05s. Since it is running 100x, this equates
+#             to a total runtime of 5s as specified in the lab """
+#             
+#             time.sleep(0.05)
+#      
