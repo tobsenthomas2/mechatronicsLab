acdb78e493f6cff3fce8b81652a548c472467642
sydneygoth <sgothenquist@hotmail.com>
2023-02-01 14:50:57 -0800

    Comments Added
    These are the comments

--- Lab1/src/encoder_reader.py
+++ Lab1/src/encoder_reader.py
@@ -13,20 +13,30 @@ class EncoderClass:
         Creates a motor driver by initializing GPIO
         pins and turning off the motor for safety. 
         @param en_pin (There will be several pin parameters)
+        In1pin and In2pin are used to define the pins (B6/B7 or C6/C7). 
+        TimerNR sets the timer channel based on the pins (TCh 4 or 8)
         """
         self.IN1_Pin=in1pin
         self.IN2_Pin=in2pin
         self.counterTimer=timerNR
-        #self.deltaTime=
         self.totalPos
         self.prevCount=0
         
         
-        
+        """!This sets encoder channels A and B to recieve data from Pyb, for
+        In1 and In2 pins which can be set based on the specific pins, and
+        motors being used
+        """
         enc_chA = pyb.Pin(self.IN1_Pin, pyb.Pin.IN)
-
         enc_chB = pyb.Pin (self.IN2_Pin, pyb.Pin.IN)
 
+        """!Enables the timer to count w/ a prescaler of 0 and a period
+        or range of values from 0 to hex FFFF, and allows for the Timer channel
+        to be specified
+        
+        Sets ch1 to timer channel 1  to read A&B for encoder A corresponding to channel A
+        which is the same for ch2
+        """
         timer= pyb.Timer (self.counterTimer, prescaler=0, period=0xFFFF)
         ch1=timer.channel (1, pyb.Timer.ENC_AB, pin=enc_chA)
         ch2=timer.channel (2, pyb.Timer.ENC_AB, pin=enc_chB)
@@ -36,12 +46,10 @@ class EncoderClass:
 
     def read (self):
         """!
-        This method sets the duty cycle to be sent
-        to the motor to the given level. Positive values
-        cause torque in one direction, negative values
-        in the opposite direction.
-        @param level A signed integer holding the duty
-               cycle of the voltage sent to the motor 
+        This method checks the counter value - the previous counter value
+        and comparing it to our "threshold". This checks for overflow.
+        If less than the threshold, we assume this is counting up and has overflowed.
+        We can scale this properly and vice versa for counting down.
         """
         print("counter " +str(self.counter.counter())+" for encoderTim: " +str(self.counterTimer))
         if (self.counter.counter()-self.prevCount <-threshold):
@@ -55,6 +63,11 @@ class EncoderClass:
         self.prevCount=self.counter.counter()
         
     def zero(self):
+        """!
+        This method zeroes our counter value: our readable counter value
+        is self.counter, using the .counter(0) we can reset this value
+        and also reset the total position to 0
+        """
         print("reset counter to 0")
         self.counter.counter(0)
         self.totalPos=0
--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -6,9 +6,21 @@ import motorDriverClassTobi
 
 
 if __name__ == "__main__":
+    """!
+        This is our main, from here we can call MotorDriver() and EncoderClass()
+        classes and specify the specific parameters based on what pins we are 
+        using.
+        From EncoderClass() we call: In1pin and In2pin to define the pins (B6/B7 or C6/C7). 
+        And the TimerNR for the timer channel based on the In pins (TCh 4 or 8)
+        From MotorDriver() we call: the pin to enable to motor (EN_Pin)
+        and set the torque control pins (PB4/5 or PA0/1) as well as the corresponding
+        timer (3 or 5)
+        We can also call the set_duty_cycle() class to set a specific duty cycle that will
+        run the motor in the correct direction at a specific speed
+        """
     Motor1=MotorDriver(pyb.Pin.board.PA10,pyb.Pin.board.PB4,pyb.Pin.board.PB5,3)
     Motor1.set_duty_cycle(90)
-    encoder=EncoderClass(pyb. Pin.board. PB6,pyb. Pin.board. PB7,4)
+    encoder=EncoderClass(pyb.Pin.board.PB6,pyb.Pin.board.PB7,4)
     while(1):
          encoder.read()
          time.sleep(0.5)
